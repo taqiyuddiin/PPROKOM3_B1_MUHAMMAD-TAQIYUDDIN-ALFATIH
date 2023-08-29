f5728b962da2fa0598f3a0349db6fbd1a7b7081a
taqiyuddiin <143482785+taqiyuddiin@users.noreply.github.com>
2023-08-29 15:48:05 +0700

    latihan 7

---
+++ main (8).py
@@ -0,0 +1,27 @@
+z = ['1', '3', '2', '4', 'Alice', 'Bob']
+z.sort(key = str)
+print(z)
+
+print("Hello there!\nHow are you?\nI\'m doing fine.")
+
+multi_line = """Hello there!
+How are you?
+I'm fine."""
+print(multi_line)
+
+spam = ' Hello World '
+spam.strip()
+print(spam)
+spam.lstrip()
+print(spam)
+spam.rstrip()
+print(spam)
+
+print(', '.join(['cats', 'rats', 'bats']))
+print(' '.join(['My', 'name', 'is', 'Simon']))
+print('ABC'.join(['My', 'name', 'is', 'Simon']))
+print('My name is Simon'.split())
+print('MyABCnameABCisABCSimon'.split('ABC'))
+print('My name is Simon'.split('m')) 
+
+
